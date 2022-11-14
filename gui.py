805786d59b57dc5c73775bd3349a908d80e65fd0
Soo <soojlee0106@einsis.com>
2022-11-14 22:12:36 +0900

    Add html elements to gui

--- gui.py
+++ gui.py
@@ -6,10 +6,27 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import streamlit as st
 import seaborn as sn
 import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-st.text("Anti-Bait")
+st.set_page_config(page_title="Anti-Bait", page_icon="https://img.icons8.com/cotton/512/news.png", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+
+components.html(
+    """
+    <div style= "color: black; font-weight: bold; text-align: center; font-size: 70px; font-family: Trebuchet MS; text-shadow: 2px 2px 6px #808080;" >
+    Anti
+    </div>
+    <div style= "color: white; font-weight: bold; text-align: center; font-size: 35px; font-family: Trebuchet MS; text-shadow: 2px 2px 6px #808080;" >
+    X
+    </div>
+    <div style= "color: white; font-weight: bold; text-align: center; font-size: 70px; font-family: Trebuchet MS; text-shadow: 2px 2px 6px #808080;" >
+    Bait
+    </div>
+    """,
+    height=250,
+)
 
 df = pd.read_csv('./news.csv')
 
@@ -39,6 +56,28 @@ score = accuracy_score(y_test, y_pred)
 st.text("This algorithm is intended to discern the validity of a news article.")
 st.text(f'Accuracy of algorithm: {round(score*100,2)}%')
 
+components.html(
+    """
+    <div>
+    </div>
+    """,
+    height=50,
+)
+
+
+user_input = st.text_input(
+    "Enter the headline of the article you want to detect.")
+data = tfidf_vectorizer.transform([user_input]).toarray()
+st.text("The following article is " + pac.predict(data)[0])
+
+components.html(
+    """
+    <div>
+    </div>
+    """,
+    height=50,
+)
+
 matrix = confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL'])
 # 589 true positives, 587 true negatives, 42 false positives, and 49 false negatives
 df_cm = pd.DataFrame(matrix, index=["FAKE", "REAL"], columns=[
@@ -46,9 +85,3 @@ df_cm = pd.DataFrame(matrix, index=["FAKE", "REAL"], columns=[
 sn.set(font_scale=1.4)
 sn.heatmap(df_cm, annot=True, annot_kws={"size": 16})
 st.pyplot()
-
-
-user_input = st.text_input(
-    "Enter the headline of the article you want to detect.")
-data = tfidf_vectorizer.transform([user_input]).toarray()
-st.text("The following article is " + pac.predict(data)[0])
